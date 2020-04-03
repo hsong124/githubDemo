60e094adf542da1143f53e91b6555ab8fe0cb34e
hsong124 <heather.song@cfacorp.com>
2020-04-03 10:08:37 -0400

    update addnum

--- utils.py
+++ utils.py
@@ -2,13 +2,13 @@
 silly utilities to give us some code to work on
 '''
 
-def add_nums(*args):     
+def add_nums(*args):
     '''
     receives any number of arguments
     sums them up and then returns the sum
     NOTE: Doesn't check that they are numbers (yet)
     '''
     sum = 0
-    for num in args:
-        sum += num
+    # for num in args:
+    #     sum += num
     return sum
